78bd151aa00a917c3bb01c814264d2a737ea7ef0
Damian Galeano <dj.damian.b@hotmail.com>
2023-05-11 21:04:29 +0200

    String functions

---
+++ main.js
@@ -0,0 +1,129 @@
+// var name = 'Damian'
+
+// let user = "damian"
+// var name = 'Massimo'
+
+
+// console.log(name)
+// console.log(user)
+
+
+// var name, city, age, number;
+
+// name = "damian"
+
+// city = "bilbao"
+// age = 35
+// number = 9
+
+// console.log(city)
+// console.log(age)
+// console.log(number)
+
+// let age = 12;
+// console.log(age)
+
+// age = 35
+// console.log(age)
+
+
+// let age = 34
+
+// var age = 9
+// var age = 35
+// console.log(age)
+
+//Some comments 
+
+
+/*
+
+another way of the comments
+
+*/
+
+//Boolean
+
+// var truthy = true;
+// var notruthy = false;
+
+// console.log(truthy)
+// console.log(notruthy)
+
+
+// Null
+
+// var nully = null
+// console.log(nully)
+
+// // Undefined 
+
+// var notDefined;
+// console.log(notDefined)
+
+
+// // String
+
+// var name = "Damian"
+// var nameTwo = "Damian Galeano"
+
+// console.log(name)
+// console.log(nameTwo)
+
+// var user = {
+//     name: 'Damian',
+//     age: 35,
+//     city: 'Bilbao',
+//     grades: {
+//         math: 90,
+//         science: 100,
+//         lenguageArts: 99
+//     }
+// }
+
+// user.age = 18
+// user.grades.music = 100
+// user.grades.inglsh = 97
+// console.log(user)
+// console.log(typeof 12)
+// console.log(typeof 'Astros')
+// console.log(typeof true)
+// console.log(typeof age)
+// console.log(typeof age)
+
+
+// var ageOne = 12
+// String(ageOne)
+// console.log(ageOne.toString())
+
+// var ageTwo = "33"
+// Number(ageTwo)
+// console.log(ageTwo)
+
+
+
+// console.log(parseInt(12, "estacion"))
+// console.log(parseInt(1, 2, 2, 2))
+// console.log(parseInt('33.5'))
+// console.log(parseFloat("22.4"))
+// console.log(Number('500') + Number(12))
+// console.log(Number(true))
+// console.log(Number(false))
+
+// var str = "The quick brown fox jumped over the lazy dog"
+
+// console.log(str.length)
+// console.log(str.charAt(11))
+
+// console.log(str.concat("run run run"))
+// console.log(str.includes("quick"))
+// console.log(str.startsWith("The quick"))
+// console.log(str.endsWith("dog"))
+
+var str = "The quick brown fox jumped over the lazy dog"
+
+console.log(str.repeat(3))
+console.log(str.match(/((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}/));
+console.log('(555)555-5555'.match(/((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}/));
+
+console.log(str.replace('fox', 'cow'))

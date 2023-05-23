f45cb461bf2e882e91fab50b1e35958e28390d4b
Damian Galeano <dj.damian.b@hotmail.com>
2023-05-23 23:00:14 +0200

    Promises 2

---
+++ api.js
@@ -0,0 +1,5 @@
+console.log('Starting fetch call');
+const postPromise = fetch('https://api.dailysmarty.com/posts');
+console.log('After fetch call');
+console.log(postPromise);
+console.log('After program has run');
